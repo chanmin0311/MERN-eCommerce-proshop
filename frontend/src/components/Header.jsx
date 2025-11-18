0b33b61c3dd66bb04dfe355932c0f6e785d484a1
chanmin0311 <ckstj1500@gmail.com>
2025-11-18 16:35:13 +0900

    refactor:  Use Link tags instead of a tags

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,25 +1,32 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
 import logo from "../assets/logo.png";
 const Header = () => {
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="md" coollapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="/">
-                        <img src={logo} alt="ProShop" />
-                        ProShop
-                    </Navbar.Brand>
+                    <LinkContainer to="/">
+                        <Navbar.Brand>
+                            <img src={logo} alt="ProShop" />
+                            ProShop
+                        </Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-conraits="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
-                            <Nav.Link href="/cart">
-                                <FaShoppingCart /> Cart
-                            </Nav.Link>
-                            <Nav.Link herf="login">
-                                <FaUser />
-                                Sign In
-                            </Nav.Link>
+                            <LinkContainer to="/cart">
+                                <Nav.Link>
+                                    <FaShoppingCart /> Cart
+                                </Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/login">
+                                <Nav.Link herf="login">
+                                    <FaUser />
+                                    Sign In
+                                </Nav.Link>
+                            </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
