9a5a10d59a6efba304bd3dd742f3dbb0ef470b73
chanmin0311 <ckstj1500@gmail.com>
2025-11-18 16:10:19 +0900

    feat: Add logo in navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,15 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
-
+import logo from "../assets/logo.png";
 const Header = () => {
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="md" coollapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="/">ProShop</Navbar.Brand>
+                    <Navbar.Brand href="/">
+                        <img src={logo} alt="ProShop" />
+                        ProShop
+                    </Navbar.Brand>
                     <Navbar.Toggle aria-conraits="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
