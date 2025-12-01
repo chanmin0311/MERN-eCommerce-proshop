9e167b154671fa3616e124b0ac291785589afe7e
chanmin0311 <ckstj1500@gmail.com>
2025-12-02 02:51:03 +0900

    feat(db): add dummy data

---
+++ backend/data/products.js
@@ -0,0 +1,76 @@
+const products = [
+    {
+        name: "Airpod Wireless Bluetooth Headphones",
+        image: "/images/airpods.jpg",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis dignissimos amet blanditiis sapiente, magnam doloremque quaerat aliquam quas explicabo aperiam maxime autem veritatis, ut suscipit perspiciatis rem laboriosam dolor eum.",
+        brand: "Apple",
+        category: "Electronics",
+        price: 89.99,
+        countInStock: 10,
+        rating: 4.5,
+        numReviews: 12,
+    },
+    {
+        name: "Alexa AI Embedded Speaker",
+        image: "/images/alexa.jpg",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic suscipit dignissimos et corrupti, corporis deleniti exercitationem vel tenetur sapiente velit quae praesentium porro in eligendi. Error repudiandae ex magnam ipsum.",
+        brand: "amazon",
+        category: "Electronics",
+        price: 298.98,
+        countInStock: 14,
+        rating: 3.9,
+        numReviews: 43,
+    },
+    {
+        name: "Camera",
+        image: "/images/camera.jpg",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo deleniti quibusdam odio perspiciatis corporis non ipsa quidem adipisci, magnam quia maxime, sapiente illo quasi, nesciunt optio voluptatum! Commodi, ea perspiciatis.",
+        brand: "Cannon",
+        category: "Electronics",
+        price: 69.99,
+        countInStock: 4,
+        rating: 4.6,
+        numReviews: 20,
+    },
+    {
+        name: "mouse is cute",
+        image: "/images/mouse.jpg",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo deleniti quibusdam odio perspiciatis corporis non ipsa quidem adipisci, magnam quia maxime, sapiente illo quasi, nesciunt optio voluptatum! Commodi, ea perspiciatis.",
+        brand: "logitech",
+        category: "Electronics",
+        price: 16.64,
+        countInStock: 8,
+        rating: 5.0,
+        numReviews: 1,
+    },
+    {
+        name: "IPhone that's it",
+        image: "/images/phone.jpg",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo deleniti quibusdam odio perspiciatis corporis non ipsa quidem adipisci, magnam quia maxime, sapiente illo quasi, nesciunt optio voluptatum! Commodi, ea perspiciatis.",
+        brand: "Apple",
+        category: "Electronics",
+        price: 199.99,
+        countInStock: 120,
+        rating: 4.2,
+        numReviews: 83,
+    },
+    {
+        name: "PlayStation PS5",
+        image: "/images/playstation.jpg",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo deleniti quibusdam odio perspiciatis corporis non ipsa quidem adipisci, magnam quia maxime, sapiente illo quasi, nesciunt optio voluptatum! Commodi, ea perspiciatis.",
+        brand: "IDK",
+        category: "Electronics",
+        price: 89.99,
+        countInStock: 10,
+        rating: 4.5,
+        numReviews: 12,
+    },
+];
+
+export default products;
